9f416a8e1942d219909e5a7713a7668fc30417ea
meilihao <563278383@qq.com>
2020-05-02 12:08:11 +0800

    .

--- example/6.rw_records/write-records-share.sh
+++ example/6.rw_records/write-records-share.sh
@@ -4,4 +4,4 @@ as write-record.s -o write-record.o
 as read-record.s -o read-record.o
 ld -shared write-record.o read-record.o -o librecord.so
 as write-records.s -o write-records.o
-ld --dynamic-linker /lib64/ld-linux-x86-64.so.2 -L . -lrecord write-records.o -o write-records.out 
+ld --dynamic-linker /lib64/ld-linux-x86-64.so.2 -L . -lrecord write-records.o -o write-records.out # -L : 告诉ld在指定目录下查找so, 也可指定LD_LIBRARY_PATH来解决.
