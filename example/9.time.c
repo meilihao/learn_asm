a90bdb320a2c7392f7f925d5f5b6fab75c66281e
meilihao <563278383@qq.com>
2020-09-22 23:05:22 +0800

    + get time

---
+++ example/9.time.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <time.h> 
+
+void printTime(time_t tt) {
+	struct tm *t;
+
+	t = localtime(&tt); 
+	printf("time:%d:%d:%d:%d:%d:%d:\n",t->tm_year+1900,t->tm_mon+1,t->tm_mday,t->tm_hour, t->tm_min,t->tm_sec);
+}
+
+int main() {   
+	time_t tt; //int型数值
+	printTime(time(NULL));
+
+	tt = 0;
+
+	// Method 1: pass a pointer to a time_t (long long) to return time in
+    // sys_time = syscall(201, *long long)
+	time_t *input = &tt;
+	printf("%p\n", input);
+	asm volatile(
+		"movq $201,%%rax\n\t"// 使用%rax传递系统调用号201
+		"movq %0,%%rdi\n\t"// 系统调用传递第一个参数使用rdi寄存器
+		"syscall\n\t" // 触发系统调用
+		:"=m"(input)
+		);
+	printTime(tt);
+	
+	//  Method 2: pass zero as the time_t parameter. sys_time returns
+    //  value in rax .
+    //  sys_time = syscall(201, *long long = 0)
+	tt = 0;
+	asm volatile(
+		"movq $201,%%rax\n\t"
+		"xorq %%rdi,%%rdi\n\t"
+		"syscall\n\t"
+		"movq %%rax,%0;\n\t"
+		:"=m"(tt)
+		);
+	printTime(tt);
+
+	return 1; 
+}
--- example/README.md
+++ example/README.md
@@ -8,6 +8,7 @@
 6. rw_records : 读写简单记录
 7. call-libc.s : 链接libc
 8. simple-memory-manager.s : 一个简单的内存管理器
+9. 内嵌汇编获取时间
 
 > 文件后缀是att的是AT&T语法, 没有则默认是Intel语法, 32表示是针对x83 32位系统的汇编. example初始时使用了AT&T语法, 以后仅提供Intel语法, 且仅针对x86_64. att语法版本可用`objdump -d program_name`获取.
 > cpuid2_*.s : 说明32/64 bit ABI差异.
