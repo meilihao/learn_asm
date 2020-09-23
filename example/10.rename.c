341c27bcaa9cbcecefebcee485a53c4c1a6933f3
meilihao <563278383@qq.com>
2020-09-23 21:44:48 +0800

    .

--- example/10.rename.c
+++ example/10.rename.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+
 
 void rename_normal(){
 	int ret;
-	char *oldname="h.c";
-	char *newname="n.c";
+	char *oldname="h0.c";
+	char *newname="h1.c";
 
 	ret=rename(oldname, newname);
 	if(ret==0){
@@ -13,10 +16,24 @@ void rename_normal(){
 	}
 }
 
+void rename_syscall(){
+	int ret;
+	char oldname[]="h1.c";
+	char newname[]="h2.c";
+
+	ret=syscall(SYS_rename,oldname, newname); // syscall from unistd.h; SYS_rename from "sys/syscall.h"
+	if(ret==0){
+		printf("rename successfully\n");
+	}else{
+		printf("rename faild\n");
+	}
+}
+
+
 void rename_asm(){
 	int ret;
-	char *oldname="n.c";
-	char *newname="h1.c";
+	char *oldname="h2.c";
+	char *newname="h3.c";
 
 	asm volatile(
 		"movq $82,%%rax\n\t"
@@ -37,6 +54,7 @@ void rename_asm(){
 int main()
 {
 	rename_normal();
+	rename_syscall();
 	rename_asm();
 
 	return 0;
